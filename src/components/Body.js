9ff24cdde65dd990f506b6bb7b1979be5a2e1c9b
LakshmanGottapu <sailakshman120@gmail.com>
2023-08-02 19:38:20 +0530

    added toggle functionality to top rated button and optimised serach functionality on top of top rated toggle on

--- src/components/Body.js
+++ src/components/Body.js
@@ -9,30 +9,43 @@ const Body = ()=>{
     let [restaurantsList, setrestaurantsList] = useState([]);
     let [filteredRestaurants,setFilteredRestaurants] = useState([]);
     const [searchText,setSearchText] = useState("");
+    const [filterFlag, setFilterFlag] = useState(false);
     useEffect(()=>{
         setTimeout(fetchData,150);
     },[])
     const fetchData = async () => {
         const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=16.7280962&lng=81.0780596&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
         const json = await data.json();
-        console.log(json);
         const restaurants = json.data?.cards[5]?.card?.card?.gridElements?.infoWithStyle?.restaurants;
         const moreRestaurants = json.data?.cards[2]?.card?.card?.gridElements?.infoWithStyle?.restaurants;
-        restaurants.push(...moreRestaurants);
-        console.log(restaurants);
+        //restaurants.push(...moreRestaurants);
         setrestaurantsList(restaurants);
         setFilteredRestaurants(restaurants);
     };
     function search(){
-        if(searchText)
-        setFilteredRestaurants(restaurantsList.filter(restaurant=>restaurant.info.name.toLowerCase().includes(searchText.toLowerCase())));
+        if(searchText){
+            if(filterFlag)
+            setFilteredRestaurants(filteredRestaurants.filter(restaurant=>restaurant.info.name.toLowerCase().includes(searchText.toLowerCase())));
+            else {
+                setFilteredRestaurants(restaurantsList.filter(restaurant=>restaurant.info.name.toLowerCase().includes(searchText.toLowerCase())));
+            }
+        }
     }
     const RestroCardPromoted = withLabelPromoted();
     return useOnlineStatus()?(
     restaurantsList.length === 0?(<Shimmer/>) :
     (<div className="body">
         <div className="filterbar flex bg-slate-100 my-8 h-16" >
-            <button className=" px-2 ml-24 border h-8 mt-4 border-black shadow-md bg-gray-300 rounded-lg" onClick={()=>{setFilteredRestaurants(restaurantsList.filter(restaurant=>restaurant.info.avgRating>4));}}>Top Rated</button>
+            <button className=" px-2 ml-24 border h-8 mt-4 border-black shadow-md bg-gray-300 rounded-lg" onClick={()=>{
+                if(!filterFlag){
+                    setFilteredRestaurants(restaurantsList.filter(restaurant=>restaurant.info.avgRating>4));
+                    setFilterFlag(true);
+                }
+                else {
+                        setFilteredRestaurants(restaurantsList);
+                        setFilterFlag(false);
+                    }
+                }}>Top Rated</button>
             <div className="searchbar ml-96 p-4" >
                 <input className="border border-black rounded-lg" type="text" value={searchText} onChange={(e)=>{
                     setSearchText(e.target.value);
@@ -46,8 +59,11 @@ const Body = ()=>{
                     console.log(restaurant.info.name);
                     return (<RestroCardPromoted key={restaurant.info.id} props={restaurant}></RestroCardPromoted>);
                 }
-                else 
+                else {
+                    console.log(restaurant.info.id);
                     return (<RestroCard key={restaurant.info.id} restaurant={restaurant.info}></RestroCard>);
+                }
+                    
             })
             } 
         </div>
